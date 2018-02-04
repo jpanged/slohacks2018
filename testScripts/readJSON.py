e40389e532305af5bd3d190317e9e6fbab711b5d
RussellCaletena <RussellCaletena@gmail.com>
2018-02-03 16:59:47 -0800

    Add json to root

---
+++ testScripts/readJSON.py
@@ -0,0 +1,12 @@
+import json
+
+
+jsonFile = open('blockBounds.json', 'r')
+jsonStr = jsonFile.read()
+print (jsonStr)
+#jsonData = json.loads(jsonStr)
+
+#print (jsonData)
+#
+# # datastore = json.loads
+# print (type(jsonData))
