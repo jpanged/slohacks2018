6cd11f0feafa37e579b184ff69bf0eba93abf18b
RussellCaletena <RussellCaletena@gmail.com>
2018-02-04 01:49:19 -0800

    Add models to admin site for Dom

--- receiptRecognition/shopperHelper/admin.py
+++ receiptRecognition/shopperHelper/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import user
+from .models import item, user, group, receipt, itemOnReceipt
 
+admin.site.register(item)
 admin.site.register(user)
+admin.site.register(group)
+admin.site.register(receipt)
+admin.site.register(itemOnReceipt)
