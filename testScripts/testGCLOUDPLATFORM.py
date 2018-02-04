947e5c1ba5f16133bf6cf7a9fdead41a0b49e144
alexgarcia98 <haloalex68@gmail.com>
2018-02-03 18:07:46 -0800

    fixed (?) loop for generating tuples (again) plsfor the last time

--- testScripts/testGCLOUDPLATFORM.py
+++ testScripts/testGCLOUDPLATFORM.py
@@ -36,14 +36,14 @@ blockBounds.write(str(response))
 tupList = []
 
 for index, response in enumerate(response.text_annotations):
-    x1 = response[index].bounding_poly.vertices[0].x
-    y1 = response[index].bounding_poly.vertices[0].y
-    x2 = response[index].bounding_poly.vertices[1].x
-    y2 = response[index].bounding_poly.vertices[1].y
-    x3 = response[index].bounding_poly.vertices[2].x
-    y3 = response[index].bounding_poly.vertices[2].y
-    x4 = response[index].bounding_poly.vertices[3].x
-    y4 = response[index].bounding_poly.vertices[3].y
+    x1 = response.bounding_poly.vertices[0].x
+    y1 = response.bounding_poly.vertices[0].y
+    x2 = response.bounding_poly.vertices[1].x
+    y2 = response.bounding_poly.vertices[1].y
+    x3 = response.bounding_poly.vertices[2].x
+    y3 = response.bounding_poly.vertices[2].y
+    x4 = response.bounding_poly.vertices[3].x
+    y4 = response.bounding_poly.vertices[3].y
 
     p1 = (x1, y1)
     p2 = (x2, y2)
