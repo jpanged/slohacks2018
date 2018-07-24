12f2c649bd5587ac62d21e9c0c142f383bc7e75a
AbdallahAbuHashem <aabuhash@stanford.edu>
2018-07-24 14:34:38 -0700

    Finished first screen looks

--- SplittyMobile/App.js
+++ SplittyMobile/App.js
@@ -20,18 +20,14 @@ export default class BadInstagramCloneApp extends Component {
             }}
             style = {styles.preview}
             type={RNCamera.Constants.Type.back}
-            flashMode={RNCamera.Constants.FlashMode.on}
+            flashMode={RNCamera.Constants.FlashMode.off}
             permissionDialogTitle={'Permission to use camera'}
             permissionDialogMessage={'We need your permission to use your camera phone'}
         />
-        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
         <TouchableOpacity
             onPress={this.takePicture.bind(this)}
             style = {styles.capture}
-        >
-            <Text style={{fontSize: 14}}> SNAP </Text>
-        </TouchableOpacity>
-        </View>
+        />
       </View>
     );
   }
@@ -49,20 +45,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: 'black'
+    backgroundColor: 'black',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
   preview: {
-    flex: 1,
+    position: 'absolute',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
     justifyContent: 'flex-end',
     alignItems: 'center'
   },
   capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20
+    backgroundColor: 'transparent',
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    borderColor: '#A8A8A8',
+    borderWidth: 7,
+    margin: 40
   }
 });
